39010f375de2c44f530d01c339a085ad02c6a071
SebastianTabbia <tabbiasebastian@gmail.com>
2023-03-02 18:14:16 -0300

    se modifica seccion bancos

--- main.js
+++ main.js
@@ -39,3 +39,61 @@
  jsArrow.onclick = function() {
   navLinks.classList.toggle("show3");
  }
+
+ const color=document.getElementById('fondo_color')
+ const numero=document.getElementById('numero')
+ 
+let cantidad=0
+ let tiempo=setInterval(() => {
+     cantidad+=1
+     color.style.height=`${cantidad}%`
+     numero.textContent=cantidad
+     if(cantidad===100){
+         clearInterval(tiempo)
+     }
+ }, 100);
+
+  // CONTADOR
+
+  addEventListener('DOMContentLoaded', ()=>{
+    const contadores = document.querySelectorAll('.contador_cantidad');
+    const velocidad = 1000;
+
+    const animarContadores = () =>{
+        for(const contador of contadores){
+            const actualizar_contador =() =>{
+                let cantidad_maxima = +contador.dataset.cantidadTotal,
+                valor_actual = +contador.innerText,
+                incremento = cantidad_maxima / velocidad;
+                if(valor_actual < cantidad_maxima){
+                    contador.innerText = Math.ceil(valor_actual + incremento)
+                    setTimeout((actualizar_contador), 5);
+                }else{
+                    contador.innerText = cantidad_maxima;
+
+                }
+            }
+            actualizar_contador();
+        }
+    }
+
+    const mostrarContadores = elementos =>{
+        elementos.forEach(element => {
+            if(element.isIntersecting){
+                   element.target.classList.add('animar');
+                   element.target.classList.remove('ocultar');
+                   setTimeout(animarContadores, 300)
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(mostrarContadores, {
+        threshold: 0.75 //0 - 1 
+    })
+
+    const elementosHTML = document.querySelectorAll('.contador')
+    elementosHTML.forEach(elementoHTML =>{
+        observer.observe(elementoHTML)
+    })
+    
+})
