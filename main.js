19317ea2e92996854f5e188ce10e6c6e711d9784
SebastianTabbia <tabbiasebastian@gmail.com>
2023-03-13 12:57:54 -0300

    se agrega seccion reposera

--- main.js
+++ main.js
@@ -54,37 +54,3 @@ let cantidad=0
  }, 100);
 
   // CONTADOR MESA PICNIC 1
-
-  let cont1=document.getElementById('cont1'),
-  cont2=document.getElementById('cont2'),
-  cont3=document.getElementById('cont3')
-  
-  let cant1=0,cant2=0,cant3=0,
-  tiem=100
-
- let tiempo1= setInterval(()=>{
-cont1.textContent=cant1+=1
-
-if(cant1===170)
-{
-    clearInterval(tiempo1)
-}
-  },'${tiem}');
-
-  let tiempo2= setInterval(()=>{
-    cont2.textContent=cant2+=1
-    
-    if(cant2===4100)
-    {
-        clearInterval(tiempo2)
-    }
-      },'${tiem}');
-
-      let tiempo3= setInterval(()=>{
-        cont3.textContent=cant3+=1
-        
-        if(cant3===4)
-        {
-            clearInterval(tiempo3)
-        }
-          },'${tiem}');
